b1afdce782419f71d1e4fa906271a73bd285ac79
jR1P621 <jonrippe@gmail.com>
2022-02-03 19:48:14 -0900

    initial commit

---
+++ react-task-tracker/src/App.js
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
+
+function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Test1',
+      day: 'day1',
+      reminder: true,
+    }
+  ])
+
+  //add task
+  const addTask = (task) => {
+    console.log(task);
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
+
+  //delete task
+  const deleteTask = (id) => {
+    console.log('delete', id);
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  //toggle reminder
+  const toggleReminder = (id) => {
+    console.log(id);
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
+  }
+
+  return (
+    <div className="container">
+      <Header onAdd={() => setShowAddTask(!showAddTask)} />
+      {showAddTask && <AddTask onAdd={addTask} />}
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : "No Tasks"}
+    </div>
+  );
+}
+
+export default App;
---
+++ react-task-tracker/src/components/Button.js
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types'
+
+const Button = ({ color, text }) => {
+    return <div>
+        <button style={{ backgroundColor: color }} className='btn'>{text}</button>
+    </div>;
+};
+
+Button.defaultProps = {
+    color: 'steelblue',
+}
+
+Button.propTypes = {
+    text: PropTypes.string,
+    color: PropTypes.string,
+    onClick: PropTypes.func,
+}
+export default Button;
